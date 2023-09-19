ed8af4e7037e79d7745275854c33447e3731c3fd
rshimokura <rshimokura@itrealize.co.jp>
2023-09-19 13:09:52 +0900

    update

--- Package.swift
+++ Package.swift
@@ -12,12 +12,13 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "LibITR",
-            targets: ["LibITR"]),
+            targets: ["LibITR"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/robb/Cartography", from: "4.0.0")
+        .package(url: "https://github.com/robb/Cartography.git", from: "4.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
