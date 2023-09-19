96f17c1c8c7c281d3f7e5e941988228f8048baf5
rshimokura <rshimokura@itrealize.co.jp>
2023-09-19 16:54:31 +0900

    u

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-//        .binaryTarget(name: "LibITR", path: "LibITR.xcframework"),
-        .binaryTarget(name: "LibITR", url: "https://github.com/rshimokura/LibITR-ForDistribution/archive/refs/tags/1.2.3.zip", checksum: "af6a3f6029a42ad9b3f46676b52a40fad15506127c3b9867afcb098a6cb52978"),
+        .binaryTarget(name: "LibITR", path: "LibITR.xcframework"),
+//        .binaryTarget(name: "LibITR", url: "https://github.com/rshimokura/LibITR-ForDistribution/archive/refs/tags/1.2.3.zip", checksum: "af6a3f6029a42ad9b3f46676b52a40fad15506127c3b9867afcb098a6cb52978"),
     ]
 )
