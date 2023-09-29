f58bb18258f55084ff58bf75f36215d1669e0fd0
rshimokura <rshimokura@itrealize.co.jp>
2023-09-29 17:28:54 +0900

    update

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
 //        .binaryTarget(name: "AlyciaITR", path: "AlyciaITR.xcframework"),
-        .binaryTarget(name: "AlyciaITR", url: "https://github.com/rshimokura/LibITR-ForDistribution/releases/download/1.3.2/AlyciaITR.xcframework.zip", checksum: "def6cb8700ff016d4a16eb84aeda19811d53d9a1857e2b65e08ce2208481c468")
+        .binaryTarget(name: "AlyciaITR", url: "https://github.com/rshimokura/LibITR-ForDistribution/releases/download/1.3.4/AlyciaITR.xcframework.zip", checksum: "def6cb8700ff016d4a16eb84aeda19811d53d9a1857e2b65e08ce2208481c468")
 //                      "https://github.com/rshimokura/AlyciaITR/releases/download/0.0.13/AlyciaITR.xcframework.zip", checksum: "c7aed3aeb2b41a03ab60808de2180029c09801bf57b1ba6e3c125502bd94f2e3")
     ]
 )
