572d5903644ade35f90c6478dc8a06b980b268e1
rshimokura <rshimokura@itrealize.co.jp>
2023-09-29 16:27:08 +0900

    add Alycia

---
+++ AlyciaITR.xcframework/ios-arm64/AlyciaITR.framework/Headers/AlyciaITR.h
@@ -0,0 +1,18 @@
+//
+//  AlyciaITR.h
+//  AlyciaITR
+//
+//  Created by Shimokura on 2023/09/27.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AlyciaITR.
+FOUNDATION_EXPORT double AlyciaITRVersionNumber;
+
+//! Project version string for AlyciaITR.
+FOUNDATION_EXPORT const unsigned char AlyciaITRVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlyciaITR/PublicHeader.h>
+
+
--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(name: "LibITR", targets: ["LibITR"])
+        .library(name: "AlyciaITR", targets: ["AlyciaITR"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,7 +20,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "LibITR", path: "LibITR.xcframework"),
+        .binaryTarget(name: "AlyciaITR", path: "AlyciaITR.xcframework"),
 //        .binaryTarget(name: "LibITR", url: "https://github.com/rshimokura/LibITR-ForDistribution/archive/refs/tags/1.2.3.zip", checksum: "af6a3f6029a42ad9b3f46676b52a40fad15506127c3b9867afcb098a6cb52978"),
     ]
 )
